20535179f33ebc0b388b135ae0497c40cd144f5f
Aleksandr <aleksandr.evlanicov@gmail.com>
2023-01-26 13:50:23 +0300

    task

--- task/Program.cs
+++ task/Program.cs
@@ -1,8 +1,24 @@
-﻿string[] text = new string [3] { "hello", "world", "!!!" };
+﻿string[] text = new string [5] { "hello", "world", "hi", "!!!", "0" };
+string[] array = new string[text.Length];
 
-for(int i = 0; i < text.Length; i++)
+int count = 0;
+for (int i = 0; i < text.Length; i++)
 {
-    Console.Write(text[i] + " ");
+    if (text[i].Length <= 3)
+    {
+        array[count] = text[i]; 
+        count++;
+        
+    }
     
-}
 
+}  
+void PrintArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+PrintArray(array);
